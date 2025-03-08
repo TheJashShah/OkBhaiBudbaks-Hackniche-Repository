2a20a72d0740dd6215b200097ebbcfb428f1110a
reyansh2005 <reyanshmehta2005@gmail.com>
2025-03-08 23:34:15 +0530

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ChatbotIcon from './pages/ChatBot'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ChatbotIcon />
   </StrictMode>,
 )
