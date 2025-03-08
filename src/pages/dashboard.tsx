df12bdcabadbff398177f899bbacad0667860755
vedaant <vedaant.mahale@gmail.com>
2025-03-08 13:26:31 +0530

    dashboard fixing

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -5,7 +5,7 @@ import Typography from '@mui/joy/Typography';
 import Card from '@mui/joy/Card';
 
 const Dashboard = () => {
-    const data = [
+    const datanew = [
         {
           src: 'https://images.unsplash.com/photo-1502657877623-f66bf489d236',
           title: 'Night view',
@@ -29,11 +29,11 @@ const Dashboard = () => {
         <h1 className="text-xl font-bold text-blue-600 md:block hidden">Best<span className="text-black">BuyCo</span></h1>
         <h1 className="text-xl font-bold text-blue-600 block md:hidden">BB</h1>
         <nav className="mt-6 space-y-4 w-full">
-          <a className="block text-gray-700 font-semibold p-2 rounded-lg hover:bg-gray-200 flex items-center justify-center md:justify-start" href="#">
+          <a className=" text-gray-700 font-semibold p-2 rounded-lg hover:bg-gray-200 flex items-center justify-center md:justify-start" href="#">
             <span className="md:hidden">🏬</span>
             <span className="hidden md:block">Popular Products</span>
           </a>
-          <a className="block bg-blue-600 text-white p-2 rounded-lg flex items-center justify-center md:justify-start" href="#">
+          <a className=" bg-blue-600 text-white p-2 rounded-lg flex items-center justify-center md:justify-start" href="#">
             <span className="md:hidden">🔍</span>
             <span className="hidden md:block">Explore New</span>
           </a>
@@ -42,10 +42,10 @@ const Dashboard = () => {
       {/* Main Content */}
       <div className="flex-1 flex flex-col">
         {/* Navbar */}
-        <div className="bg-white p-4 h-30 w-[71vw] flex flex-col items-center md:flex-row md:h-20 md:justify-between ">
+        <div className="bg-white p-4 h-30 md:w-[85vw] w-[70vw] flex flex-col items-center md:flex-row md:h-20 md:justify-between ">
             <div className="w-full flex justify-between flex-col md:flex-row ">
             <h2 className="text-xl font-bold text-center m-auto md-m-auto md:ml-0 md:text-left">Explore</h2>
-            <input className="w-[200px] md:w-full max-w-xs h-10 rounded-full  m-auto md:m-0 md:ml-auto md:mt-0 mt-2 bg-gradient-to-br from-blue-50 to-slate-100 text-center" placeholder="Search..." />
+            <input className="w-[200px] md:w-full max-w-xs h-10 rounded-full m-auto md:m-0 md:ml-auto md:mt-0 mt-2 bg-gradient-to-br from-blue-50 to-slate-100 text-center" placeholder="Search..." />
             </div>
         </div>
         {/* Product Grid */}
@@ -57,7 +57,10 @@ const Dashboard = () => {
             gap: 1,
             py: 1,
             overflow: 'auto',
-            width: '85vw',
+            width: 'auto',
+            '@media (max-width: 600px)' :{
+                width: '70vw'
+            },
             height: 'auto',
             scrollSnapType: 'x mandatory',
             '& > *': {
@@ -66,7 +69,7 @@ const Dashboard = () => {
             '::-webkit-scrollbar': { display: 'none' },
         }}
         >
-        {data.map((item) => (
+        {datanew.map((item) => (
             <Card
             key={item.title}
             variant="outlined"
